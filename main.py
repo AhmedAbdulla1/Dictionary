1ebd80330cf09ce317ab53ab54ccb7f8294175eb
AhmedAbdulla1 <97388228+AhmedAbdulla1@users.noreply.github.com>
2022-11-08 22:38:50 +0200

    my projct

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+f = open('Dictionary.txt', 'r')
+content = f.read()
+dic_to_list = content.split()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def search_index(item, _list1):
+    if len(_list1) == 0:
+        return
+    else:
+        n = len(_list1)
+        mid = n // 2
+        if _list1[mid] == item:
+            return mid
+        else:
+            if item > _list1[mid]:
+                search_index(item, _list1[mid + 1:])
+            else:
+                search_index(item, _list1[:mid])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def by(item, _list1):
+    if len(_list1) == 0:
+        return False
+    else:
+        n = len(_list1)
+        mid = n // 2
+        if _list1[mid] == item:
+            return True
+        else:
+            if item > _list1[mid]:
+                return by(item, _list1[mid + 1:])
+            else:
+                return by(item, _list1[:mid])
+
+
+print(search_index('b', dic_to_list))
+# ascii_ = "abcdefghijklmnopqrstuvwxvz"
+# for i in ascii_:
+#     index.append(search_index(i, dic_to_list))
+#
+# print(index)
+# a = {
+#
+# }
+#
+# print(by("done", _list))
+# # text = input("enter any text ")
+# # _listOfText = text.split()
+# # for word in _listOfText:
+# #     if by(word, _list):
+# #         print(word)
+# #     else:
+# #         print('error')
+# # print(by(_listOfText[0], _list))
+# # print(_list)
